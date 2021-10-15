0311503878d9c267959d75f7d79d712ab589d62d
Shrikant Kumar <shrikantkumar@Pro.local>
2021-10-15 19:00:35 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const mongoDB = require('mongodb');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    const userId = new mongoDB.ObjectId('61696c84554d5f0e4165c604');
+    //console.log(userId);
+    User.findById(userId)
+        .then(user => {
+            req.user = new User(user.name, user.email, user.cart, user._id);
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoConnect(() => {
+    app.listen(3000);
+});
